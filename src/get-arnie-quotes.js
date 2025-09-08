c7aa0cd641bb8232f8ebcd8754a92f7d7f365423
vikhyatsharma43 <vikhyatsharma43@gmail.com>
2025-09-08 19:49:38 +1000

    getArnieQuotes implementation

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,8 +1,26 @@
 const { httpGet } = require('./mock-http-interface');
 
+const HTTP_OK = 200;
+
 const getArnieQuotes = async (urls) => {
-  // TODO: Implement this function.
-  // return results;
+  // fetch all URLs in parallel for speed
+  const promises = urls.map(async (url) => {
+    
+    const response = await httpGet(url);
+    const body = JSON.parse(response.body);
+    const message = body.message;
+
+    let result;
+    if (response.status === HTTP_OK) {
+      result = { 'Arnie Quote': message };
+    } else {
+      result = { 'FAILURE': message };
+    }
+
+    return result;
+  });
+
+  return Promise.all(promises);
 };
 
 module.exports = {
